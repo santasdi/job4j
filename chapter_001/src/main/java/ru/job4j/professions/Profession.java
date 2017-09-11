a0af7d7cae135e998e010278863b84f1ac25a38b
santasdi <bartenev92@gmail.com>
2017-09-11 23:35:38 +0300

    task's name [#6837]

--- chapter_001/src/main/java/ru/job4j/professions/Profession.java
+++ chapter_001/src/main/java/ru/job4j/professions/Profession.java
@@ -1,7 +1,34 @@
 package ru.job4j.professions;
 
 /**
- * Created by Артем on 11.09.2017.
+ * Realization of professions.
+ *
+ * @author Artem Bartenev (bartenev92@gmail.com)
+ * @version $Id$
+ * @since 11.09.2017
  */
 public class Profession {
+    private String name;
+    private String type;
+    private int age;
+
+    public Profession() {
+    }
+
+    public Profession(String name, String type, int age) {
+        this.name = name;
+        this.type = type;
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+    public int getAge() {
+        return age;
+    }
+
 }
