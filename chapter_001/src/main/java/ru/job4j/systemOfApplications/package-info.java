aa4ae391b9f1368e519c6fb49d7817dd4667cf50
santasdi <bartenev92@gmail.com>
2017-09-21 22:47:04 +0300

    task's name [#396]

---
+++ chapter_001/src/main/java/ru/job4j/systemOfApplications/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Classes for using items.
+ *
+ * @author Artem Bartenev (bartenev92@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
+package ru.job4j.systemOfApplications;
---
+++ chapter_001/src/test/java/ru/job4j/systemOfApplications/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Test methods for system of application.
+ *
+ * @author Artem Bartenev (bartenev92@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+package ru.job4j.systemOfApplications;
