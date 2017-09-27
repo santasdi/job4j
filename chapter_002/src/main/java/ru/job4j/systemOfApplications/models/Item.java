abf5f042e086b3190e828a1ecbd31d001bdc4eed
santasdi <bartenev92@gmail.com>
2017-09-27 22:44:17 +0300

    task's name [#396]

---
+++ chapter_002/src/main/java/ru/job4j/systemOfApplications/models/Item.java
@@ -0,0 +1,60 @@
+package ru.job4j.systemOfApplications.models;
+
+/**
+ * Class item description.
+ * @author Artem Bartenev (bartenev92@gmail.com)
+ * @version $Id$
+ * @since 21.09.2017
+ */
+public class Item {
+    private String name;
+    private String id;
+    private String desc;
+    private long created;
+    private String comments;
+
+    public Item(String name, String desc, long created) {
+        this.name = name;
+        this.desc = desc;
+        this.created = created;
+
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getId() {
+        return id;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setCreated(long created) {
+        this.created = created;
+    }
+
+    public long getCreated() {
+        return created;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/systemOfApplications/tracker/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * //source code for application.
+ * @author Artem Bartenev (bartenev92@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+package ru.job4j.systemOfApplications.tracker;
