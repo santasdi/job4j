cefb06fa4eac23672c2d3607b0864170736ac06c
santasdi <bartenev92@gmail.com>
2017-10-02 00:18:49 +0300

    task's name [#784] refactoring and adding

--- chapter_002/src/main/java/ru/job4j/application/tracker/ConsoleInput.java
+++ chapter_002/src/main/java/ru/job4j/application/tracker/ConsoleInput.java
@@ -1,105 +1,27 @@
 package ru.job4j.application.tracker;
 
-import ru.job4j.application.models.Item;
-
-import java.util.Arrays;
 import java.util.Scanner;
 
 /**
- * Created by Артем on 24.09.2017.
+ * ConsoleInput.
+ * @author Artem Bartenev (bartenev92@gmail.com)
+ * @version $Id$
+ * @since 21.09.2017
  */
-public class ConsoleInput implements Input {
+public class ConsoleInput implements Input{
     /**
-     * Constanta ADD.
-     */
-    private static final String ADD = "0";
-    /**
-     * Constanta SHOW.
-     */
-    private static final String SHOW = "1";
-    /**
-     * Constanta EDIT.
-     */
-    private static final String EDIT = "2";
-    /**
-     * Constanta DELETE.
-     */
-    private static final String DELETE = "3";
-    /**
-     * Constanta FINDID.
-     */
-    private static final String FINDID = "4";
-    /**
-     * Constanta FINDNAME.
-     */
-    private static final String FINDNAME = "5";
-    /**
-     * Constanta EXIT.
-     */
-    private static final String EXIT = "6";
-    /**
-     * Object scanner.
+     * Scanner input stream.
      */
     private Scanner scanner = new Scanner(System.in);
     /**
-     * Object tracker.
-     */
-    private Tracker tracker = new Tracker();
-
-    /**
-     * Print menu.
-     */
-    public void menu() {
-        System.out.println("0. Add new Item\n"
-                + "1. Show all items\n"
-                + "2. Edit item\n"
-                + "3. Delete item\n"
-                + "4. Find item by Id\n"
-                + "5. Find items by name\n"
-                + "6. Exit Program\n"
-                + "Select:");
-        }
-
-    /**
-     * Result of users choice.
-     * @param answer is answer of user
+     * Asking question.
+     * @param question is question.
+     * @return answer of user.
      */
     @Override
-    public void ask(String answer) {
-        if (ADD.equals(answer)) {
-            System.out.println("Enter name");
-            String name = scanner.nextLine();
-            System.out.println("Enter description");
-            String desc = scanner.nextLine();
-            Item item = new Item(name, desc, 3);
-            tracker.add(item);
-            System.out.println("Id of item: " + item.getId());
-        } else if (SHOW.equals(answer)) {
-            System.out.println(Arrays.toString(tracker.findAll()));
-        } else if (EDIT.equals(answer)) {
-            System.out.println("Enter id");
-            String id = scanner.nextLine();
-            System.out.println("Enter new name of item");
-            String name = scanner.nextLine();
-            System.out.println("Enter new description of item");
-            String desc = scanner.nextLine();
-            Item item = new Item(name, desc, 14);
-            tracker.update(id, item);
-        } else if (DELETE.equals(answer)) {
-            System.out.println("Enter id");
-            String id = scanner.nextLine();
-            tracker.delete(tracker.findById(id));
-        } else if (FINDID.equals(answer)) {
-            System.out.println("Enter id");
-            String id = scanner.nextLine();
-            Item item = tracker.findById(id);
-            System.out.println(item);
-        } else if (FINDNAME.equals(answer)) {
-            System.out.println("Enter name");
-            String name = scanner.nextLine();
-            System.out.println(Arrays.toString(tracker.findByName(name)));
-        } else if (EXIT.equals(answer)) {
-            System.out.println("Exit");
-        }
+    public String ask(String question) {
+        System.out.println(question);
+        String line = scanner.nextLine();
+        return line;
     }
 }
--- chapter_002/src/main/java/ru/job4j/application/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/application/tracker/StartUI.java
@@ -1,29 +1,175 @@
 package ru.job4j.application.tracker;
-
-import java.util.Scanner;
+import ru.job4j.application.models.Item;
+import java.util.Arrays;
 /**
- * Methods to start apllication.
+ * StartUI.
  * @author Artem Bartenev (bartenev92@gmail.com)
  * @version $Id$
- * @since 24.09.2017
+ * @since 1.10.2017
  */
 public class StartUI {
     /**
-     * Starting of program.
-     * @param args is arguments
+     * Menu.
+     */
+    private final String menu = "0. Add new Item\n"
+            + "1. Show all items\n"
+            + "2. Edit item\n"
+            + "3. Delete item\n"
+            + "4. Find item by Id\n"
+            + "5. Find items by name\n"
+            + "6. Exit Program\n"
+            + "Select:";
+    /**
+     * Exit.
+     */
+    private static final String EXIT = "6";
+    /**
+     * ADD.
+     */
+    private static final String ADD = "0";
+    /**
+     * Show all.
+     */
+    private static final String SHOW_ALL = "1";
+    /**
+     * Update item.
+     */
+    private static final String EDIT_ITEM = "2";
+    /**
+     * Delete item.
+     */
+    private static final String DELETE_ITEM = "3";
+    /**
+     * Find by id.
+     */
+    private static final String FIND_BY_ID = "4";
+    /**
+     * Find by name.
+     */
+    private static final String FIND_BY_NAME = "5";
+    /**
+     * Input.
+     */
+    private ConsoleInput input;
+    /**
+     * Tracker.
+     */
+    private Tracker tracker;
+
+    /**
+     * Constructor.
+     */
+    public StartUI() {
+        this.input = new ConsoleInput();
+        this.tracker = new Tracker();
+    }
+
+    /**
+     * Adding item.
+     *
+     * @param tracker is tracker object.
+     * @param input is input object.
+     */
+    private void add(Tracker tracker, ConsoleInput input) {
+        String name = input.ask("Enter name :");
+        String desc = input.ask("Enter description :");
+        tracker.add(new Item(name, desc, 11));
+    }
+
+    /**
+     * Show all items.
+     *
+     * @param tracker tracker.
+     */
+    private void showAll(Tracker tracker) {
+        System.out.println(Arrays.toString(tracker.findAll()));
+    }
+
+    /**
+     * Update item.
+     *
+     * @param tracker is tracker object.
+     * @param input is input object
+     */
+    private void editItem(Tracker tracker, ConsoleInput input) {
+        String id = input.ask("Enter id :");
+        tracker.update(id, tracker.findById(id));
+    }
+
+    /**
+     * Delete item.
+     *
+     * @param tracker is tracker object.
+     * @param input is input object
+     */
+    private void deleteItem(Tracker tracker, ConsoleInput input) {
+        String id = input.ask("Enter id :");
+        tracker.delete(tracker.findById(id));
+    }
+
+    /**
+     * Find item by id.
+     *
+     * @param tracker is tracker object.
+     * @param input is input object
+     */
+    private void findById(Tracker tracker, ConsoleInput input) {
+        String id = input.ask("Enter id :");
+        Item item = tracker.findById(id);
+        System.out.println(item);
+    }
+
+    /**
+     * Find item by name.
+     *
+     * @param tracker is tracker object.
+     * @param input  is input object
+     */
+    private void findByName(Tracker tracker, ConsoleInput input) {
+        String name = input.ask("Enter name :");
+        Item[] item = tracker.findByName(name);
+        System.out.println(Arrays.toString(item));
+    }
+
+    /**
+     * Main method.
+     * @param args is args
      */
     public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    ConsoleInput console = new ConsoleInput();
-    boolean status = true;
-        while (status) {
-            console.menu();
-            String answer = scanner.nextLine();
-            if (answer.equals("6")) {
-                status = false;
-            } else {
-                console.ask(answer);
+        new StartUI().start();
+    }
+
+    /**
+     * Starting of application.
+     */
+    public void start() {
+        String answer;
+        while (true) {
+            answer = input.ask(menu);
+            if (EXIT.equals(answer)) {
+                break;
             }
+            switch (answer) {
+                case ADD:
+                    this.add(this.tracker, this.input);
+                    break;
+                case SHOW_ALL:
+                    this.showAll(this.tracker);
+                    break;
+                case EDIT_ITEM:
+                    this.editItem(this.tracker, this.input);
+                    break;
+                case DELETE_ITEM:
+                    this.deleteItem(tracker, input);
+                    break;
+                case FIND_BY_ID:
+                    this.findById(tracker, input);
+                    break;
+                case FIND_BY_NAME:
+                    this.findByName(tracker, input);
+                    break;
+            }
+
         }
     }
 }
